b0ae73eb7276554a49b7ee51ba348a2875317c0f
luke1491 <lukaszcieplicki@gmail.com>
2024-01-02 16:04:16 +0100

    Fix authOptions export

--- src/app/api/profile/image/route.js
+++ src/app/api/profile/image/route.js
@@ -1,7 +1,7 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
 export async function PUT(req) {
   try {
--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { User } from "@/app/models/User";
 
 export async function PUT(req) {
